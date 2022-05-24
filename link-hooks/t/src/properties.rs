59a567b1f3bbaf7e58d9a8d23030e77dd381a46a
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-05-24 14:17:02 +0100

    fixup! link-hooks: roundtrip property tests
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- link-hooks/t/src/properties.rs
+++ link-hooks/t/src/properties.rs
@@ -3,33 +3,11 @@
 
 use proptest::prelude::*;
 
+use link_hooks::{Data, IsZero, Track};
+use radicle_git_ext as ext;
 use test_helpers::roundtrip;
 
 use crate::gen::{data::gen_data, track::gen_track};
-
-// #[test]
-// fn roundtrip() {
-//     let track = link_hooks::Track {
-//         urn: link_identities::urn::Urn {
-//             id: "d238753c66687be890be6e434c132457422889a5"
-//                 .parse::<radicle_git_ext::Oid>()
-//                 .unwrap(),
-//             path: Some(radicle_git_ext::RefLike::try_from("1mvwk").unwrap()),
-//         },
-//         peer: "hyb558i4w81syeoest7oabc8ia8cdn9piremk7uz9eqxwqj575fc8c"
-//             .parse()
-//             .map(Some)
-//             .unwrap(),
-//         old: "dcf5b16e76cce7425d0beaef62d79a7d10fce1f5"
-//             .parse::<radicle_git_ext::Oid>()
-//             .unwrap(),
-//         new: "4d4f8376cd8d37a935b3d5dd0eb855c40b519341"
-//             .parse::<radicle_git_ext::Oid>()
-//             .unwrap(),
-//     };
-//     print!("WTF: {}\n", track);
-//     roundtrip::str(track)
-// }
 
 proptest! {
     #[test]
@@ -39,7 +17,76 @@ proptest! {
 
     #[test]
     fn roundtrip_track(track in gen_track()) {
-        print!("WTF: {}", track);
         roundtrip::str(track)
+    }
+
+    #[test]
+    fn track_updated(track in gen_track()) {
+        prop_track_created(track.clone());
+        prop_track_deleted(track.clone());
+        prop_track_changed(track.clone());
+    }
+
+        #[test]
+    fn data_updated(data in gen_data()) {
+        prop_data_created(data.clone());
+        prop_data_deleted(data.clone());
+        prop_data_changed(data.clone());
+    }
+}
+
+fn prop_track_created(track: Track<ext::Oid>) {
+    if !track.new.is_zero() {
+        let track = Track {
+            old: git2::Oid::zero().into(),
+            ..track
+        };
+
+        assert!(track.is_created());
+    }
+}
+
+fn prop_track_deleted(track: Track<ext::Oid>) {
+    if !track.old.is_zero() {
+        let track = Track {
+            new: git2::Oid::zero().into(),
+            ..track
+        };
+
+        assert!(track.is_deleted());
+    }
+}
+
+fn prop_track_changed(track: Track<ext::Oid>) {
+    if !track.old.is_zero() && !track.new.is_zero() {
+        assert!(track.is_changed());
+    }
+}
+
+fn prop_data_created(data: Data<ext::Oid>) {
+    if !data.new.is_zero() {
+        let data = Data {
+            old: git2::Oid::zero().into(),
+            ..data
+        };
+
+        assert!(data.is_created());
+    }
+}
+
+fn prop_data_deleted(data: Data<ext::Oid>) {
+    if !data.old.is_zero() {
+        let data = Data {
+            new: git2::Oid::zero().into(),
+            ..data
+        };
+
+        assert!(data.is_deleted());
+    }
+}
+
+fn prop_data_changed(data: Data<ext::Oid>) {
+    if !data.old.is_zero() && !data.new.is_zero() {
+        assert!(data.is_changed());
     }
 }
