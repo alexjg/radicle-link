3a67508ad34024cd9a9b1605c55683dcf7176c79
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-05-26 09:23:15 +0100

    WIP: draft notifier
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- link-hooks/src/lib.rs
+++ link-hooks/src/lib.rs
@@ -2,9 +2,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pub mod data;
+use std::fmt;
+
 pub use data::Data;
 
 pub mod track;
+use link_identities::urn::HasProtocol;
+use multihash::Multihash;
 pub use track::Track;
 
 pub enum Event {
@@ -15,6 +19,9 @@ pub enum Event {
 pub trait IsZero {
     fn is_zero(&self) -> bool;
 }
+
+/// End of transimission character.
+pub const EOT: i32 = 0x04;
 
 #[cfg(feature = "git")]
 mod git {
@@ -36,14 +43,72 @@ mod git {
     }
 }
 
-pub trait Hook {
-    type Arg;
-    type Error;
+pub trait Handle {
+    type WriteError: std::error::Error + Send + Sync + 'static;
 
-    fn run<I>(&self, arg: I) -> Result<(), Self::Error>
-    where
-        I: Iterator<Item = Self::Arg>;
+    fn write(&mut self, bs: &[u8]) -> Result<(), Self::WriteError>;
 }
 
-// TODO(finto): Hook execution with arguments - end of process 0x04
-// TODO(finto): Test the API surface of Data and Track
+pub trait Hook {
+    type Handle: Handle;
+    type HandleError: std::error::Error + Send + Sync + 'static;
+
+    fn handle(&self) -> Result<Self::Handle, Self::HandleError>;
+}
+
+pub struct NotifyData<R> {
+    data: Vec<Data<R>>,
+}
+
+// TODO(finto): We may want to be able to interleave notifying new updates until
+// the EOT is sent. For example, track peer A, send notification, do more
+// things, track peer B, send notification, send EOT.
+
+// TODO(finto): handle and write would probably need to be async to allow for
+// interleaving.
+
+// TODO(finto): can unify with a trait that "renders" the hook input, but this
+// trait should be sealed for Data and Track.
+impl<R> NotifyData<R>
+where
+    R: HasProtocol + fmt::Display,
+    for<'a> &'a R: Into<Multihash>,
+{
+    pub fn notify<H: Hook>(
+        &self,
+        hook: H,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let mut handle = hook.handle().map_err(Box::new)?;
+        for data in &self.data {
+            handle
+                .write(data.to_string().as_bytes())
+                .map_err(Box::new)?;
+        }
+
+        Ok(())
+    }
+}
+
+pub struct NotifyTrack<R> {
+    track: Vec<Track<R>>,
+}
+
+impl<R> NotifyTrack<R>
+where
+    R: HasProtocol + fmt::Display,
+    for<'a> &'a R: Into<Multihash>,
+{
+    pub fn notify<H: Hook>(
+        &self,
+        hook: H,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let mut handle = hook.handle().map_err(Box::new)?;
+        for track in &self.track {
+            handle
+                .write(track.to_string().as_bytes())
+                .map_err(Box::new)?;
+        }
+
+        Ok(())
+    }
+}
