69e9e2e5cc5e2eccc5b46434b53c9a7fd8043569
Alex Good <alex@memoryandthought.me>
2022-05-27 17:59:31 +0100

    Add lnk clone
    lnk clone first syncs the local monorepo state with configured seeds for
    the given URN, then checks out a working copy of the URN.
    
    If a peer ID is given `lnk clone` checks out the given peers copy. If
    not `lnk clone` will attempt to determine if there is a head the project
    delegates agree on and set `refs/namespaces/<urn>/HEAD` to this
    reference and then check this reference out to the working copy; if the
    delegates have forked `lnk clone` will print an error message with
    information on which peers are pointing at what so the user can decide
    for themselves which peer to check out.
    
    Signed-of-by: Alex Good <alex@memoryandthought.me>

--- cli/lnk-sync/src/cli/args.rs
+++ cli/lnk-sync/src/cli/args.rs
@@ -1,15 +1,24 @@
 // Copyright © 2022 The Radicle Link Contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use clap::Parser;
 use librad::git::Urn;
 
 use crate::Mode;
 
-#[derive(Clone, Debug, Parser)]
-pub struct Args {
-    #[clap(long)]
-    pub urn: Urn,
-    #[clap(long, default_value_t)]
-    pub mode: Mode,
+#[derive(Clone, Debug, clap::Subcommand)]
+pub enum Args {
+    Sync {
+        #[clap(long)]
+        urn: Urn,
+        #[clap(long, default_value_t)]
+        mode: Mode,
+    },
+    Clone {
+        #[clap(long)]
+        urn: Urn,
+        #[clap(long)]
+        path: Option<std::path::PathBuf>,
+        #[clap(long)]
+        peer: Option<librad::PeerId>,
+    },
 }
